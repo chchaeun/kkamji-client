06da3537bbb949d42f103efdc7bc6f229abf12f9
chchaeun <kce3615@naver.com>
2022-07-26 12:17:33 +0900

    Feat: 리액트 쿼리 세팅 (LEAR-65)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,21 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import Layout from "../components/layout/Layout";
-
+import Layout from "../components/layout/layout";
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 function MyApp({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Hydrate>
+    </QueryClientProvider>
   );
 }
 
