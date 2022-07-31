8b5efffab170c697fbc470a29ea5448465c92e13
chchaeun <kce3615@naver.com>
2022-07-31 16:48:03 +0900

    Feat: 문제집 상세보기 페이지 구현 (LEAR-93)

--- stores/header.ts
+++ stores/header.ts
@@ -1,6 +1,6 @@
 import { atom } from "recoil";
 
-export const showRankingState = atom({
-  key: "showRankingState",
+export const showNavState = atom({
+  key: "showNavState",
   default: false,
 });
