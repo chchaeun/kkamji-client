603bf6fc44d3e9349dab4124f875157fcab4e8a2
chchaeun <kce3615@naver.com>
2022-07-31 13:24:04 +0900

    Feat: 포스트맨 Mock 서버 연동

--- api/my-api.ts
+++ api/my-api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getCode } from "./session-code";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://a61e9270-0366-4013-a651-fbc3d46384ab.mock.pstmn.io/v1",
 });
 
 api.defaults.headers.common["code"] = getCode() || "";
