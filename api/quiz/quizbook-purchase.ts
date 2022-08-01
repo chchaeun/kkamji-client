f7151f70e70fe408d00509d141168057323b4625
chchaeun <kce3615@naver.com>
2022-08-01 16:24:04 +0900

    Remove: 사용하지 않는 파일 삭제

--- api/quiz/quizbook-purchase.ts
+++
@@ -1,5 +0,0 @@
-import api from "../my-api";
-
-export const postQuizbookPurchase = async (quizbookId: number) => {
-  return await api.post("/quizbook-purchase", { quizPackageID: quizbookId });
-};
