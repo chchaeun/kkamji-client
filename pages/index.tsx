3e36df30297fca4d9b421006f3b95834452de1f0
chchaeun <kce3615@naver.com>
2022-08-01 15:30:27 +0900

    Fix: 배포 버그 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,6 @@
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import Quizbook from "../components/quiz/quizbook";
-import { useQuery } from "@tanstack/react-query";
-import { fetchQuizbooks, IQuizbook } from "../api/quiz/quizbooks";
-import { fetchUserInfo, IUserInfo } from "../api/user/user-info";
-import {
-  fetchSubmitPeriods,
-  ISubmitPeriod,
-} from "../api/submit/submit-periods";
-import Overlay from "../components/layout/overlay";
-import QuizbookPurchaseModal from "../components/quiz/quizbook-purchase-modal";
-import { classNames } from "../styles/classname-maker";
-import SubmitPeriods from "../components/submit/submit-periods";
+import { useEffect } from "react";
 
 const Home: NextPage = () => {
   const router = useRouter();
