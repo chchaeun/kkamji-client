87509efb7740a5f3924d05a29be0317e9e7ba8e6
chchaeun <kce3615@naver.com>
2022-08-02 21:40:09 +0900

    Fix: 라우트 에러 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,10 @@ import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 const Home: NextPage = () => {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/quizbooks?week=1");
+  }, [router]);
   return <div></div>;
 };
 
