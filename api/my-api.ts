8a0fe242524a8d7dfb724f8981708161c09b2251
chchaeun <kce3615@naver.com>
2022-08-04 14:42:09 +0900

    Chore: 테스트용 baseURL 설정

--- api/my-api.ts
+++ api/my-api.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 import { getCode } from "./session-code";
 
 const api = axios.create({
-  baseURL: "https://a61e9270-0366-4013-a651-fbc3d46384ab.mock.pstmn.io/v1",
+  // baseURL: "https://a61e9270-0366-4013-a651-fbc3d46384ab.mock.pstmn.io/v1",
+  baseURL: "http://localhost:8080",
 });
 
 api.defaults.headers.common["code"] = getCode() || "";
