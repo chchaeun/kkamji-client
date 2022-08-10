6ae88e5640ddd6eb06a447c84681c7f547b8ee27
chchaeun <kce3615@naver.com>
2022-08-10 23:58:07 +0900

    Design: 문제 제출 모달 반응형 CSS 구현 (LEAR-133)

--- components/quiz/submit-success-modal.tsx
+++ components/quiz/submit-success-modal.tsx
@@ -18,15 +18,15 @@ function SubmitSuccessModal({
   };
 
   return (
-    <div className="grid grid-rows-6 grid-cols-10 items-center justify-center gap-5 fixed w-2/5 h-1/3 left-1/2 top-1/4 -translate-x-1/2 bg-white rounded-lg z-10">
+    <div className="grid grid-rows-6 grid-cols-10 items-center justify-center gap-5 fixed w-2/5 h-1/3  sm:w-3/4 sm:h-2/5 left-1/2 top-1/4 -translate-x-1/2 bg-white rounded-lg z-10">
       <Icon
         icon="bi:x-lg"
         color="black"
         height="30"
         onClick={onMainClick}
-        className="row-start-1 col-start-10 mt-5 cursor-pointer"
+        className="row-start-1 col-start-10 sm:col-start-9  mt-5 cursor-pointer"
       />
-      <p className="row-start-3 col-start-3 col-span-6 flex flex-col items-center text-lg">
+      <p className="row-start-3 col-start-3 col-span-6 sm:col-start-2 sm:col-span-8 flex flex-col items-center sm:items-start text-lg sm:text-base">
         <span>문제가 제출되었습니다.</span>
         {quizCurrentSubmit === 8 ? (
           <span>
@@ -51,7 +51,7 @@ function SubmitSuccessModal({
         <button
           type="button"
           onClick={onAgainClick}
-          className="row-start-5 col-start-4 col-span-4 bg-[#5c3cde] hover:bg-[#4026ab] text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline cursor-pointer"
+          className="row-start-5 col-start-4 col-span-4 sm:col-start-3 sm:col-span-6 bg-[#5c3cde] hover:bg-[#4026ab] text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline cursor-pointer"
         >
           한 문제 더 제출하기
         </button>
