f1abf450f682263aeeb948f1438fbd63bdee5e20
chchaeun <kce3615@naver.com>
2022-08-13 18:23:57 +0900

    Delete: API URL 노출 삭제

--- api/my-api.ts
+++ api/my-api.ts
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://a61e9270-0366-4013-a651-fbc3d46384ab.mock.pstmn.io/v1",
-  // baseURL: "http://localhost:8080/v1",
-  // baseURL: "https://prod.kkamjidot.com/v1",
-  baseURL: "https://dev.kkamjidot.com/v1",
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
 });
 
 export default api;
