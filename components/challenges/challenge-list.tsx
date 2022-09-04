8a393b27fb60a983c14e1bc5879fd91517f04e39
chchaeun <kce3615@naver.com>
2022-09-04 11:46:15 +0900

    Feat: 챌린지 접근 필터링 (LEAR-211)

--- components/challenges/challenge-list.tsx
+++ components/challenges/challenge-list.tsx
@@ -10,25 +10,31 @@ function ChallengeList({ challenges }: Props) {
   return (
     <div className="grid grid-cols-3 gap-10 sm:flex sm:flex-col">
       {challenges?.map((challenge) => (
-        <Link
-          href={`/challenges/${challenge.challengeId}`}
-          key={challenge.challengeId}
-        >
-          <div className="col-span-1 flex flex-col gap-3 transition ease-in-out hover:-translate-y-1 duration-300 cursor-pointer">
-            <img
-              src={challenge.imageUrl}
-              className="w-96 h-52 object-cover rounded-2xl"
-            />
-            <div className="flex justify-between items-center">
-              <div>
-                <div className="text-lg font-semibold">{challenge.title}</div>
-                <div className="text-gray-700">
-                  {challenge.professorName} 교수님
+        <div key={challenge.challengeId}>
+          {challenge.applicationStatus === "ACCEPTED" && (
+            <Link
+              href={`/challenges/${challenge.challengeId}`}
+              key={challenge.challengeId}
+            >
+              <div className="col-span-1 flex flex-col gap-3 transition ease-in-out hover:-translate-y-1 duration-300 cursor-pointer">
+                <img
+                  src={challenge.imageUrl}
+                  className="w-96 h-52 object-cover rounded-2xl"
+                />
+                <div className="flex justify-between items-center">
+                  <div>
+                    <div className="text-lg font-semibold">
+                      {challenge.title} {challenge.chapter}기
+                    </div>
+                    <div className="text-gray-700">
+                      {challenge.professorName} 교수님
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
-          </div>
-        </Link>
+            </Link>
+          )}
+        </div>
       ))}
     </div>
   );
