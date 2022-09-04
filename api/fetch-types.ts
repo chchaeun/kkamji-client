0202ad3f4c8dde698d903192df490d9548d4583c
chchaeun <kce3615@naver.com>
2022-09-04 11:29:19 +0900

    Delete: 챕터와 문제집 내용 삭제 (LEAR-211)

--- api/fetch-types.ts
+++
@@ -1,9 +0,0 @@
-export interface IFetchChapter {
-  chapterId?: string;
-}
-export interface IFetchQuizbook extends IFetchChapter {
-  quizbookId?: string;
-}
-export interface IFetchQuiz extends IFetchQuizbook {
-  quizId: string;
-}
