84a159d0638690f92a9875e926703d4b6a4a21ed
chchaeun <kce3615@naver.com>
2022-09-04 22:27:58 +0900

    Design: 퀴즈 table 모바일 overflow 수정 (LEAR-211)

--- components/quizzes/quiz/quiz-list.tsx
+++ components/quizzes/quiz/quiz-list.tsx
@@ -11,45 +11,47 @@ function QuizList({ quizzes }: Props) {
   const challengeId = String(router.query.cid);
 
   return (
-    <table className="table w-full">
-      <thead>
-        <tr>
-          <th className="bg-gray-200">번호</th>
-          <th className="bg-gray-200">제목</th>
-          <th className="bg-gray-200">작성자</th>
-          <th className="bg-gray-200">주차</th>
-          <th className="bg-gray-200">해결</th>
-        </tr>
-      </thead>
-      <tbody>
-        {quizzes?.map((quiz) => (
-          <Link
-            href={`/challenges/${challengeId}/quizzes/${quiz.quizId}/?week=${
-              router.asPath.split("?week=")[1]
-            }`}
-            key={quiz.quizId}
-          >
-            <tr className="cursor-pointer">
-              <td>{quiz.quizId}</td>
-              <td>{quiz.quizTitle}</td>
-              <td>{quiz.writerName}</td>
-              <td>{quiz.quizWeek}</td>
-              {quiz.solveAnswer ? (
-                <td>
-                  <Icon
-                    icon="bi:patch-check-fill"
-                    color="#5c3cde"
-                    height={24}
-                  />
-                </td>
-              ) : (
-                <td></td>
-              )}
-            </tr>
-          </Link>
-        ))}
-      </tbody>
-    </table>
+    <div className="w-full sm:overflow-x-scroll">
+      <table className="table w-full">
+        <thead>
+          <tr>
+            <th className="bg-gray-200">번호</th>
+            <th className="bg-gray-200">제목</th>
+            <th className="bg-gray-200">작성자</th>
+            <th className="bg-gray-200">주차</th>
+            <th className="bg-gray-200">해결</th>
+          </tr>
+        </thead>
+        <tbody className="">
+          {quizzes?.map((quiz) => (
+            <Link
+              href={`/challenges/${challengeId}/quizzes/${quiz.quizId}/?week=${
+                router.asPath.split("?week=")[1]
+              }`}
+              key={quiz.quizId}
+            >
+              <tr className="cursor-pointer sm:text-sm">
+                <td>{quiz.quizId}</td>
+                <td>{quiz.quizTitle}</td>
+                <td>{quiz.writerName}</td>
+                <td>{quiz.quizWeek}</td>
+                {quiz.solveAnswer ? (
+                  <td>
+                    <Icon
+                      icon="bi:patch-check-fill"
+                      color="#5c3cde"
+                      height={24}
+                    />
+                  </td>
+                ) : (
+                  <td></td>
+                )}
+              </tr>
+            </Link>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
