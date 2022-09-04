c07822e287856fcd5fcc712b42a468cb95acdae6
chchaeun <kce3615@naver.com>
2022-09-04 11:32:38 +0900

    Feat: 의견 제출 API 수정 반영 (LEAR-211)

--- api/comments/comments.ts
+++ api/comments/comments.ts
@@ -1,9 +1,10 @@
-import { IFetchQuiz } from "../fetch-types";
 import api from "../my-api";
 import { getCode } from "../session-code";
-
-export const fetchComments = async ({ quizId }: IFetchQuiz) => {
-  api.defaults.headers.common["code"] = getCode() || "";
+interface Props {
+  quizId: string;
+}
+export const fetchComments = async ({ quizId }: Props) => {
+  api.defaults.headers.common["code"] = getCode();
   const { data } = await api.get(`/quizzes/${quizId}/comments`);
   return data;
 };
