470d68a76ff80678d7f656de027f764c1d2f9641
chchaeun <kce3615@naver.com>
2022-09-26 18:01:17 +0900

    Delete: 사용하지 않는 컴포넌트 삭제

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,7 +1,6 @@
 import { useRouter } from "next/router";
 import React, { Fragment, ReactNode, useEffect } from "react";
 import Header from "./header";
-import MobileNav from "./mobile-nav";
 interface IProps {
   children: ReactNode;
 }
@@ -9,7 +8,6 @@ function Layout(props: IProps) {
   return (
     <Fragment>
       <Header />
-      <MobileNav />
       <main className="h-full">{props.children}</main>
     </Fragment>
   );
