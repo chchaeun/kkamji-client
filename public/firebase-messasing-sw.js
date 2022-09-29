27f1cc4407aa3cc34b73a66bd4b7e4700d10252e
chchaeun <kce3615@naver.com>
2022-09-29 19:18:20 +0900

    Fix: 서비스 워커 파일 이름 변경 (LEAR-275)

--- public/firebase-messasing-sw.js
+++ public/firebase-messasing-sw.js
@@ -2,15 +2,8 @@ importScripts("https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js");
 importScripts(
   "https://www.gstatic.com/firebasejs/9.10.0/firebase-messaging.js"
 );
-const firebaseConfig = {
-  apiKey: "AIzaSyAo1brz1FwHNtm3I9AGxBIe7-uKF9B6Y-k",
-  authDomain: "kkamji-e6a59.firebaseapp.com",
-  projectId: "kkamji-e6a59",
-  storageBucket: "kkamji-e6a59.appspot.com",
-  messagingSenderId: "619302817285",
-  appId: "1:619302817285:web:f7d43c92c49613c557166c",
-  measurementId: "G-602VS9X70J",
-};
+
+import { firebaseConfig } from "../utils/firebase-config";
 
 firebase.initializeApp(firebaseConfig);
 
