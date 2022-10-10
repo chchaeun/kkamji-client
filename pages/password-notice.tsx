abeb514b67bbaabb643f232cd94ee8909bbb49b3
chchaeun <kce3615@naver.com>
2022-10-10 20:16:41 +0900

    Feat: 비밀번호 보안 권장 사항 구현 (LEAR-325)

--- pages/password-notice.tsx
+++ pages/password-notice.tsx
@@ -1,23 +1,44 @@
-import React from "react";
+import { Icon } from "@iconify/react";
+import React, { useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import HeadTitle from "../components/common/Title";
 
 type PasswordValidForm = {
   password: string;
-  passwordConform: string;
+  passwordConfirm: string;
 };
 
 function PasswordNoticePage() {
+  const inputType = {
+    password: "password",
+    text: "text",
+  };
+  const [showPassword, setShowPassword] = useState(false);
+  const [showPasswordConfirm, setShowPasswordConfirm] = useState(false);
+
+  const onShowPasswordClick = () => {
+    setShowPassword((prev) => !prev);
+  };
+
+  const onShowPasswordConfirmClick = () => {
+    setShowPasswordConfirm((prev) => !prev);
+  };
+
   const {
     register,
     handleSubmit,
     formState: { errors },
+    watch,
   } = useForm<PasswordValidForm>();
 
-  const onPasswordValid: SubmitHandler<PasswordValidForm> = async (data) => {
-    const { password, passwordConform } = data;
+  const passwordField = watch("password");
 
-    if (password === passwordConform) {
+  console.log(passwordField);
+
+  const onPasswordValid: SubmitHandler<PasswordValidForm> = async (data) => {
+    const { password, passwordConfirm } = data;
+
+    if (password === passwordConfirm) {
       // mutatePasswordChange(data);
     } else {
       alert("비밀번호와 비밀번호 확인이 다릅니다.");
@@ -41,25 +62,70 @@ function PasswordNoticePage() {
           <div className="flex flex-col gap-2">
             <label className="flex flex-col">
               새 비밀번호
-              <input
-                type="password"
-                {...register("password", {
-                  required: "새 비밀번호를 입력해주세요.",
-                })}
-                className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-              />
+              <div className="flex">
+                <input
+                  type={showPassword ? inputType.text : inputType.password}
+                  {...register("password", {
+                    required: "새 비밀번호를 입력해주세요.",
+                    minLength: {
+                      value: 8,
+                      message: "최소 8자 이상의 비밀번호를 입력해주세요.",
+                    },
+                    pattern: {
+                      value: /^(?=.*\d)(?=.*[a-zA-zS]).{8,}/,
+                      message: "영문, 숫자를 혼용하여 입력해주세요.",
+                    },
+                  })}
+                  className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                />
+                <button
+                  onClick={onShowPasswordClick}
+                  type="button"
+                  className="px-2"
+                >
+                  {showPassword ? (
+                    <Icon icon="ant-design:eye-filled" color="#696969" />
+                  ) : (
+                    <Icon
+                      icon="ant-design:eye-invisible-filled"
+                      color="#696969"
+                    />
+                  )}
+                </button>
+              </div>
               <em>{errors.password?.message}</em>
             </label>
             <label className="flex flex-col">
               새 비밀번호 확인
-              <input
-                type="password"
-                {...register("passwordConform", {
-                  required: "새 비밀번호 확인을 입력해주세요.",
-                })}
-                className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-              />
-              <em>{errors.passwordConform?.message}</em>
+              <div className="flex">
+                <input
+                  type={
+                    showPasswordConfirm ? inputType.text : inputType.password
+                  }
+                  {...register("passwordConfirm", {
+                    required: "새 비밀번호 확인을 입력해주세요.",
+                    validate: (value) =>
+                      value === passwordField ||
+                      "비밀번호와 비밀번호 확인이 다릅니다.",
+                  })}
+                  className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                />
+                <button
+                  onClick={onShowPasswordConfirmClick}
+                  type="button"
+                  className="px-2"
+                >
+                  {showPasswordConfirm ? (
+                    <Icon icon="ant-design:eye-filled" color="#696969" />
+                  ) : (
+                    <Icon
+                      icon="ant-design:eye-invisible-filled"
+                      color="#696969"
+                    />
+                  )}
+                </button>
+              </div>
+              <em>{errors.passwordConfirm?.message}</em>
             </label>
           </div>
           <button
