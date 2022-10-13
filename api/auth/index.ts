4c1a1a17bea67ca389b0b25a6c7341232fd9a981
chchaeun <kce3615@naver.com>
2022-10-13 19:05:07 +0900

    Feat: 로그인 API URL 변경

--- api/auth/index.ts
+++ api/auth/index.ts
@@ -2,7 +2,10 @@ import axios from "axios";
 import { LoginProps } from "./types";
 
 const postLogin = async (loginBody: LoginProps) => {
-  return await axios.post("https://dev.kkamjidot.com/v2/user/login", loginBody);
+  return await axios.post(
+    "https://prod.kkamjidot.com/v2/user/login",
+    loginBody
+  );
 };
 
 export { postLogin };
