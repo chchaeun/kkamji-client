67b945b9761c5f98e9d4e6058b2e2d88a403d343
chchaeun <kce3615@naver.com>
2022-11-23 00:23:39 +0900

    Fix: Authorization header 함수 변경에 따른 사이드 이펙트 해결

--- api/comments/index.ts
+++ api/comments/index.ts
@@ -1,4 +1,5 @@
 import { authorizationHeader } from "../utils/authHeader";
+import { getJwtToken } from "../utils/getJwtToken";
 import { apiV1 } from "../utils/myApi";
 
 import { CommentDeleteProps, CommentUpdateProps } from "./types";
@@ -7,13 +8,13 @@ const updateComment = async ({ requestData }: CommentUpdateProps) => {
   const { commentBody, quizId } = requestData;
 
   return await apiV1.post(`/quizzes/${quizId}/comments`, commentBody, {
-    headers: authorizationHeader,
+    headers: authorizationHeader(getJwtToken()),
   });
 };
 
 const deleteComment = async ({ commentId }: CommentDeleteProps) => {
   await apiV1.delete(`/comments/${commentId}`, {
-    headers: authorizationHeader,
+    headers: authorizationHeader(getJwtToken()),
   });
 };
 
--- api/solve/index.ts
+++ api/solve/index.ts
@@ -1,4 +1,5 @@
 import { authorizationHeader } from "../utils/authHeader";
+import { getJwtToken } from "../utils/getJwtToken";
 import { apiV1, apiV2 } from "../utils/myApi";
 import { QuizAnswerProps, QuizGradeProps } from "./types";
 
@@ -6,13 +7,13 @@ const updateQuizIsSolved = async ({ quizId, answer }: QuizAnswerProps) => {
   return await apiV1.post(
     `/quizzes/${quizId}/solve`,
     { answer },
-    { headers: authorizationHeader }
+    { headers: authorizationHeader(getJwtToken()) }
   );
 };
 
 const updateQuizGrade = async ({ quizId, scoreBody }: QuizGradeProps) => {
   return await apiV2.post(`/quizzes/${quizId}/grade`, scoreBody, {
-    headers: authorizationHeader,
+    headers: authorizationHeader(getJwtToken()),
   });
 };
 
